9ab6203891ef2de20b12ab79a7c8cf217f953d6c
ayesha <ayeshaparvez0603@gmail.com>
2024-02-16 11:18:53 +0000

    first commit

---
+++ days_api/date_functions.py
@@ -0,0 +1,15 @@
+"""Functions for working with dates."""
+
+from datetime import datetime
+
+
+def convert_to_datetime(date: str) -> datetime:
+    pass
+
+
+def get_days_between(first: datetime, last: datetime) -> int:
+    pass
+
+
+def get_day_of_week_on(date: datetime) -> str:
+    pass
