6d6c67d3b42fc2b12301cfd91fe91b09cdb11467
ayesha <ayeshaparvez0603@gmail.com>
2024-02-16 12:58:07 +0000

    date functions

--- days_api/date_functions.py
+++ days_api/date_functions.py
@@ -4,12 +4,28 @@ from datetime import datetime
 
 
 def convert_to_datetime(date: str) -> datetime:
-    pass
+    try:
+        d = datetime.strptime(date, "%d.%m.%Y")
+        return d
+
+    except:
+        raise ValueError('Unable to convert value to datetime.')
 
 
 def get_days_between(first: datetime, last: datetime) -> int:
-    pass
+    if isinstance(first, datetime) and isinstance(last, datetime):
+        if first > last:
+            difference = first - last
+        else:
+            difference = last - first
+
+        return difference.days
+
+    raise TypeError('Datetimes required.')
 
 
 def get_day_of_week_on(date: datetime) -> str:
-    pass
+    if not isinstance(date, datetime):
+        raise TypeError('Datetime required.')
+    else:
+        return datetime.strftime(date, '%A')
